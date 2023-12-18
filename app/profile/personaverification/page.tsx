988d664cd4fc9bc3ce3b0f04b0f6f5f80ad70ab9
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-18 20:37:06 +0530

    Add files via upload

--- app/profile/personaverification/page.tsx
+++ app/profile/personaverification/page.tsx
@@ -1,25 +1,23 @@
 'use client';
 import React, { useEffect, useState } from 'react';
+import Persona from 'persona';
 import { callApi } from '../../_actions/personaupdateapi';
 import { useRouter } from 'next/navigation';
 
+
 const InlineInquiry = () => {
+ 
     const router = useRouter();
     const [myuserId, setMyuserId] = useState<string | null>(null);
-    var importPersona 
+
     useEffect(() => {
-        importPersona = async () => {
-            const Persona = await import('persona');
-            
-        };
-
-        importPersona();
-
         const userIdFromStorage = localStorage.getItem('userId');
         if (userIdFromStorage) {
             setMyuserId(userIdFromStorage);
         }
     }, []);
+
+ 
 
     const handleComplete = async ({ inquiryId, status, fields }: any) => {
         try {
@@ -33,16 +31,18 @@ const InlineInquiry = () => {
         }
     };
 
+
     return (
         <div className='h-screen flex justify-center'>
-            {/* Assuming Persona.Inquiry is available here */}
-            <importPersona.Persona.Inquiry
+            <Persona.Inquiry
                 templateId='itmpl_oFwr5vDFxPnJVnpKmXpgxY5x'
                 environmentId='env_3gPXHtfowwicvW8eh5GdW9PV'
+
                 onComplete={handleComplete}
             />
         </div>
     );
 };
+
 
 export default InlineInquiry;
