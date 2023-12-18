e66a2986a20365e31463a26ff074eb0c122ed515
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-18 20:21:03 +0530

    Add files via upload

--- app/profile/personaverification/page.tsx
+++ app/profile/personaverification/page.tsx
@@ -1,45 +1,47 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import Persona from 'persona';
-import { callApi } from '../../_actions/personaupdateapi';
-import { useRouter } from 'next/navigation';
+// 'use client';
+// import React, { useEffect, useState } from 'react';
+// import Persona from 'persona';
+// import { callApi } from '../../_actions/personaupdateapi';
+// import { useRouter } from 'next/navigation';
 
-// Define the InlineInquiry component
-const InlineInquiry: React.FC = () => {
+
+// const InlineInquiry = () => {
  
-    const router = useRouter();
-    const [myuserId, setMyuserId] = useState<string | null>(null);
+//     const router = useRouter();
+//     const [myuserId, setMyuserId] = useState<string | null>(null);
 
-    
+//     useEffect(() => {
+//         const userIdFromStorage = localStorage.getItem('userId');
+//         if (userIdFromStorage) {
+//             setMyuserId(userIdFromStorage);
+//         }
+//     }, []);
 
  
 
-    const handleComplete = async ({ inquiryId, status, fields }: any) => {
-        try {
-            if (status === 'completed') {
-                // await callApi(inquiryId, myuserId);
-                // router.push('/profile'); 
-            }
-        } catch (e) {
-            console.error("Error in handleComplete:", e);
-            throw Error(e);
-        }
-    };
+//     const handleComplete = async ({ inquiryId, status, fields }: any) => {
+//         try {
+//             if (status === 'completed') {
+//                 await callApi(inquiryId, myuserId);
+//                 router.push('/profile'); 
+//             }
+//         } catch (e) {
+//             console.error("Error in handleComplete:", e);
+//             throw Error(e);
+//         }
+//     };
 
-  
-    return (
-        <div className='h-screen flex justify-center'>
-            <Persona.Inquiry
-                templateId='itmpl_oFwr5vDFxPnJVnpKmXpgxY5x'
-                environmentId='env_3gPXHtfowwicvW8eh5GdW9PV'
-                onLoad={() => {
-                    console.log('Loaded inline');
-                }}
-                onComplete={handleComplete}
-            />
-        </div>
-    );
-};
 
-// Export the component
-export default InlineInquiry;
+//     return (
+//         <div className='h-screen flex justify-center'>
+//             <Persona.Inquiry
+//                 templateId='itmpl_oFwr5vDFxPnJVnpKmXpgxY5x'
+//                 environmentId='env_3gPXHtfowwicvW8eh5GdW9PV'
+//                 onComplete={handleComplete}
+//             />
+//         </div>
+//     );
+// };
+
+
+// export default InlineInquiry;
